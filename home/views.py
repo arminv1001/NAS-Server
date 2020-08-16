ae5dce52a922c496ace1d60d0b171e2551343825
Armin Vosoghi Marand <Armin Vosoghi Marand>
2020-08-16 23:04:07 +0200

    LoginFenster ohne DB

--- home/views.py
+++ home/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {'name': 'Armin'});
+
+
+def add(request):
+    val1 = request.POST['num1']
+    val2 = request.POST['num2']
+    res = val1 + val2
+    return render(request,'result.html', {'result': res})
