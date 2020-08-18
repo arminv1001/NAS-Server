ca0cf940ce730fbfc0a404292297c5e639afd5cb
Armin Vosoghi Marand <Armin Vosoghi Marand>
2020-08-18 22:59:08 +0200

    Download/Upload/Design/NavBar steht. Es fehlt Login Kontrolle

--- home/urls.py
+++ home/urls.py
@@ -4,10 +4,12 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('login', views.login, name='login'),
     path('upload', views.upload, name='upload'),
+    path('download',views.download, name='download'),
     path('admin/',admin.site.urls)
 ]
 
--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
+import os
+from django.conf import settings
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
 # Create your views here.
+login = False
+
 def home(request):
     return render(request, 'home.html', {'name': 'Armin'});
 
@@ -11,7 +15,7 @@ def login(request):
     val2 = request.POST['num2']
     if(val1 == "arminv1001"):
         if(val2 == "123456"):
-            res = "true"
+            login = True
             return render(request,'result.html')
     return render(request,'home.html',{'name': ', Passwort oder Benutzername war falsch!'})
 
@@ -23,3 +27,11 @@ def upload(request):
         name =  fs.save(upload_file.name, upload_file)
         context['url'] = fs.url(name)
     return render(request, 'result.html', context)
+
+def download(request):
+    a = "/Users/armin/Documents/Projekte/EigeneProjekte/NasServer/media"
+    liste = {}
+    liste['inhalt'] = os.listdir(a)
+    print(liste)
+    return render(request,'download.html', liste)
+#TODO Login
