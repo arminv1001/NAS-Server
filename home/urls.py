d1f6f78377dfcd78b3117540b1617156019f10bd
Armin Vosoghi Marand <Armin Vosoghi Marand>
2020-08-18 06:56:01 +0200

    Upload in Media Ordner

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,15 @@
+from django.conf import settings
 from  django.urls import path
+from django.contrib import admin
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('add', views.add, name='add')
+    path('login', views.login, name='login'),
+    path('upload', views.upload, name='upload'),
+    path('admin/',admin.site.urls)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {'name': 'Armin'});
 
 
-def add(request):
+def login(request):
     val1 = request.POST['num1']
     val2 = request.POST['num2']
     if(val1 == "arminv1001"):
         if(val2 == "123456"):
             res = "true"
             return render(request,'result.html')
-
     return render(request,'home.html',{'name': ', Passwort oder Benutzername war falsch!'})
+
+def upload(request):
+    context = {}
+    if request.method == 'POST':
+        upload_file = request.FILES['document']
+        fs = FileSystemStorage()
+        name =  fs.save(upload_file.name, upload_file)
+        context['url'] = fs.url(name)
+    return render(request, 'result.html', context)
